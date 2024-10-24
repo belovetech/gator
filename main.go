88cca61b01ff38efc7d3519554bf115ef0bfe7bd
Abeeb Raheem <belovetech@gmail.com>
2024-10-24 20:12:04 +0100

    (refactor): create parse command args function

--- main.go
+++ main.go
@@ -14,31 +14,42 @@ func loadConfig() (config.Config, error) {
 	return config.Read()
 }
 
-func main() {
+func initializeState() (*database.Queries, *config.Config, error) {
 	config, err := loadConfig()
 	if err != nil {
-		log.Fatalf("Error reading config: %v", err)
+		return nil, nil, err
 	}
 
 	db, err := sql.Open("postgres", config.DBUrl)
 	if err != nil {
-		log.Fatalf("database connection error: %v", err)
-	}
-	dbQueries := database.New(db)
-
-	appState := &state{
-		db:     dbQueries,
-		config: &config,
+		return nil, nil, err
 	}
 
-	cmds := newCommandRegistry()
+	return database.New(db), &config, nil
 
+}
+
+func parseCommandArgs() (string, []string) {
 	args := os.Args
 	if len(args) < 2 {
 		log.Fatalf("Error: no command provided")
 	}
-	cmdName, cmdArgs := args[1], args[2:]
+	return args[1], args[2:]
+}
 
+func main() {
+	dbQueries, config, err := initializeState()
+	if err != nil {
+		log.Fatalf("failed to initialize state: %v", err)
+	}
+
+	appState := &state{
+		db:     dbQueries,
+		config: config,
+	}
+
+	cmdName, cmdArgs := parseCommandArgs()
+	cmds := newCommandRegistry()
 	registerCommands(cmds)
 	runCommand(cmds, appState, cmdName, cmdArgs)
 }
